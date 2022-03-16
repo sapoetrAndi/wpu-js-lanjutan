4cfef5629efdc623445c46e416fbd2216ab512e3
andi saputra <sapoetrandi10@gmail.com>
2022-03-16 11:36:31 +0700

    promise

--- 11.1-promise/script.js
+++ 11.1-promise/script.js
@@ -38,3 +38,35 @@ janji2
 console.log("selesai");
 
 // Promise.all() berfungsi ketika kita punya banyak promise dan ingin menjalankannya sekaligus
+
+const film = new Promise( resolve => {
+    setTimeout(() => {
+        resolve([{
+            judul: 'Avengers',
+            sutradara: 'Sandhika Galih',
+            pemeran: 'Doddy, Erik',
+        }])
+    }, 1000);
+})
+
+const cuaca = new Promise( resolve => {
+    setTimeout(() => {
+        resolve([{
+            kota: 'Bandung',
+            temp: 26,
+            kondisi: 'Cerah Berawan',
+        }])
+    }, 500);
+})
+
+// film.then(response => console.log(response));
+// cuaca.then(response => console.log(response));
+
+// jika mau menjalankan promise sekaligus
+Promise.all([film, cuaca])
+    // .then(response => console.log(response));
+    .then(response => {
+        const [film, cuaca] = response;
+        console.log(film);
+        console.log(cuaca);
+    });
